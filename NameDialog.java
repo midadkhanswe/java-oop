69c2609baedaf44ad7df5de0cb22dd64cc150e53
midadkhanswe <porteboshomidad@gmail.com>
2024-09-23 03:37:34 +0600

    Add files via upload

---
+++ NameDialog.java
@@ -0,0 +1,17 @@
+package com.GUI_andGraphicsCase;
+
+import javax.swing.JOptionPane;
+
+public class NameDialog
+{
+    public static void main(String[] args)
+    {
+
+        String name = JOptionPane.showInputDialog("What is your name?");
+
+
+        String message =
+                String.format("Welcome, %s, to Java Programming!", name);
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
